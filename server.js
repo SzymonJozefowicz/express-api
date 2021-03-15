834bcffd728892b39fc4ef79ba29f0b3b70a730b
Szymon Józefowicz <szymon@statoilfuelretail.com>
2021-03-15 17:27:19 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('{\n"response":"country\n"}')
-})
+var router = express.Router();              // get an instance of the express Router
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+    res.json({ response: 'COUNTRY' });   
+});
+app.use('/api', router);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
